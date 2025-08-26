029fac6f4bd93afbedcfdf529950efbbe458d0e0
ErikDanielsson <danielsson.erik.0@gmail.com>
2025-08-26 12:56:42 +0200

    Add conversion to long for script

--- R/IBA-data_helpers.R
+++ R/IBA-data_helpers.R
@@ -7,13 +7,31 @@
 #' @param format The file ending. Defaults to 'tsv'
 #' @return A data frame of the file
 #' @export
-get_IBA_file_country <- function(fn_prefix, data_source, country = "SE", format="tsv", ...) {
+get_IBA_file_country <- function(
+    fn_prefix,
+    data_source,
+    country = "SE",
+    format="tsv",
+    long=FALSE, 
+    col_header_name=NULL,
+    val_header_name=NULL,
+    ...
+) {
    # Check that the locality looks correct
   if (country != "SE" && country != "MG") {
     message(glue("Country code '{locality}' is invalid. Please pick either 'SE' or 'MG'")) 
     return(NA)
   }
-  return(get_IBA_data_file(fn_prefix, data_source, arguments = country, format = format, ...))
+  return(get_IBA_data_file(
+    fn_prefix,
+    data_source,
+    arguments = country,
+    format = format,
+    long = long,
+    col_header_name = col_header_name,
+    val_header_name = val_header_name,
+    ...
+  ))
 }
 
 #' Get arrow table from a tabular file
@@ -25,7 +43,16 @@ get_IBA_file_country <- function(fn_prefix, data_source, country = "SE", format=
 #' @param format The file ending. Defaults to 'tsv'
 #' @return A data frame of the file
 #' @export
-get_IBA_data_file <- function(fn_prefix, data_source, arguments=NULL, format="tsv", ...) {
+get_IBA_data_file <- function(
+  fn_prefix,
+  data_source,
+  arguments=NULL,
+  format="tsv",
+  long=FALSE,
+  col_header_name=NULL,
+  val_header_name=NULL,
+  ...
+) {
   # Construct the file name
   if (!is.null(arguments)) {
     fn_prefix <- glue("{fn_prefix}_{arguments}") 
@@ -39,6 +66,15 @@ get_IBA_data_file <- function(fn_prefix, data_source, arguments=NULL, format="ts
   # Construct the file name and path 
   fn <- glue("{fn_prefix}.{format}")
   csv_fp <- path(carolus_dir("data", "IBA", data_source), fn) 
+  
+  if (long) {
+    message("File is long")
+     csv_fp <- make_file_long(
+       csv_fp, data_source, col_header_name, val_header_name
+     )
+  } else {
+    message("File is not long")
+  }
   
   return(get_arrow_file(csv_fp, data_source, ...))
 }
